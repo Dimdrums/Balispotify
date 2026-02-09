f2dfe4ec3efc2443efb4b4ddfccb0df9f3ccad83
Dim <dimnsteam@gmail.com>
2026-02-09 21:10:57 +0100

    commit 2 (Bluetooth)

--- BaliSpotify.py
+++ BaliSpotify.py
@@ -2,6 +2,38 @@ from pywinauto import Application, Desktop
 import os
 import time
 import psutil
+import subprocess
+import asyncio
+from winrt.windows.devices.radios import Radio, RadioKind, RadioState
+
+
+async def set_bluetooth(enabled: bool): # ChatGPT : Ok
+
+    radios = await Radio.get_radios_async()
+
+    for radio in radios:
+
+        if radio.kind == RadioKind.BLUETOOTH:
+
+            if enabled and radio.state != RadioState.ON:
+
+                print("Activation du Bluetooth...")
+                result = await radio.set_state_async(RadioState.ON)
+                print("Bluetooth activé")
+
+            elif not enabled and radio.state != RadioState.OFF:
+
+                print("Désactivation du Bluetooth...")
+                result = await radio.set_state_async(RadioState.OFF)
+                print("Bluetooth désactivé")
+
+            else:
+                print("Bluetooth déjà dans le bon état")
+
+            return True
+
+    print("Aucun adaptateur Bluetooth trouvé")
+    return False
 
 
 
@@ -62,15 +94,16 @@ def Wait_for_button(win,timeout,time_pace=4):
 
 ##############################################################
 
+asyncio.run(set_bluetooth(True))
 
 url="https://open.spotify.com/playlist/6MFXJRfwrMzpS0AUnbV7tu?si=97a835f88a2c4bd5"
 
-playlist_key=url.split("playlist/")[1].split("?")[0]
-#print(playlist_key)
+# playlist_key=url.split("playlist/")[1].split("?")[0]
+# #print(playlist_key)
 
-window,_=openSpotify_wait(playlist_key,10)
+# window,_=openSpotify_wait(playlist_key,10)
 
-play_button=Wait_for_button(window,5)
-#play_button.click_input()
+# play_button=Wait_for_button(window,5)
+# #play_button.click_input()
 
 
